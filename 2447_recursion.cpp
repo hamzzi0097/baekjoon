8a915d004494910e9ee1beacded08cc229d8b1a3
TaeHyun Kim <81970551+hamzzi0097@users.noreply.github.com>
2023-04-13 17:36:00 +0900

    Add files via upload

--- 2447_recursion.cpp
+++ 2447_recursion.cpp
@@ -2,40 +2,78 @@
 using namespace std;
 
 int N;
-int starArr[10000][10000] = { 0, };
 
-void checkStar(int start_row, int end_row, int start_col, int end_col) {
+/*
+star가 있으면 0
+blank면 1
+*/
+bool starArr[3000][3000] = { 0, };
+
+/*
+star => 0
+blank => 1
+*/
+// blank를 계산
+void checkBlank(int s_row, int e_row, int s_col, int e_col) {	
+	int n = (e_row - s_row) / 3;
+
+	if (n == 1)return;
+
+	for (int i = s_row + n; i < s_row + n * 2; i++) {
+		for (int j = s_col + n; j < s_col + n * 2; j++) {
+			starArr[i][j] = 1;
+		}
+	}
+
+	checkBlank(s_row, s_row + n, s_col, s_col + n);
+	checkBlank(s_row, s_row + n, s_col + n, s_col + n * 2);
+	checkBlank(s_row, s_row + n, s_col + n * 2, s_col + n * 3);
+
+	checkBlank(s_row + n, s_row + n * 2, s_col, s_col + n);
+	checkBlank(s_row + n, s_row + n * 2, s_col + n * 2, s_col + n * 3);
+
+	checkBlank(s_row + n * 2, s_row + n * 3, s_col, s_col + n);
+	checkBlank(s_row + n * 2, s_row + n * 3, s_col + n, s_col + n * 2);
+	checkBlank(s_row + n * 2, s_row + n * 3, s_col + n * 2, s_col + n * 3);
+}
+
+/*
+star => 1
+blank => 0
+*/
+// star를 계산
+void checkstar(int start_row, int end_row, int start_col, int end_col) {
 	if (end_row - start_row == 1 && end_col - start_col == 1) {
 		starArr[start_row][start_col] = 1;
 		return;
 	}
 
-	checkStar(start_row, start_row + end_row / 3, start_col, start_col + end_col / 3);
-	checkStar(start_row, start_row + end_row / 3, start_col + end_col / 3, start_col + end_col / 3 * 2);
-	checkStar(start_row, start_row + end_row / 3, start_col + end_col / 3 * 2, start_col + end_col);
-	
-	checkStar(start_row + end_row / 3, start_row + end_row / 3 * 2, start_col, start_col + end_col / 3);
-	//checkStar(end_row / 3, end_row / 3 * 2, end_col / 3, end_col / 3 * 2);
-	checkStar(start_row + end_row / 3, start_row + end_row / 3 * 2, start_col + end_col / 3 * 2, start_col + end_col);
-	
-	checkStar(start_row + end_row / 3 * 2, start_row + end_row, start_col, start_col + end_col / 3);
-	checkStar(start_row + end_row / 3 * 2, start_row + end_row, start_col + end_col / 3, start_col + end_col / 3 * 2);
-	checkStar(start_row + end_row / 3 * 2, start_row + end_row, start_col + end_col / 3 * 2, start_col + end_col);
+	int n = (end_row - start_row) / 3;
+
+	checkstar(start_row, start_row + n, start_col, start_col + n);
+	checkstar(start_row, start_row + n, start_col + n, start_col + n * 2);
+	checkstar(start_row, start_row + n, start_col + n * 2, start_col + n * 3);
+
+	checkstar(start_row + n, start_row + n * 2, start_col, start_col + n);
+	checkstar(start_row + n, start_row + n * 2, start_col + n * 2, start_col + n * 3);
+
+	checkstar(start_row + n * 2, start_row + n * 3, start_col, start_col + n);
+	checkstar(start_row + n * 2, start_row + n * 3, start_col + n, start_col + n * 2);
+	checkstar(start_row + n * 2, start_row + n * 3, start_col + n * 2, start_col + n * 3);
 }
 
 int main() {
 	cin >> N;
-	checkStar(0, N, 0,N);
+	checkstar(0, N, 0, N);
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			cout << starArr[i][j] << " ";
-			//if (starArr[i][j]==1) {
-			//	cout << "*";
-			//}
-			//else {
-			//	cout << " ";
-			//}
+			if (starArr[i][j]==1) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
 		}
 		cout << "\n";
 	}
